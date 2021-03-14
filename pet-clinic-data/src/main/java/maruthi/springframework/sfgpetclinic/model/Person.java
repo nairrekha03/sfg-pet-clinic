cadd7bb437a06415f05b68bea346aafdd81b0b73
Rekha Nair <nairrekha03@gmail.com>
2021-03-14 13:29:15 -0400

    Refactoring to common curd interface. Closes #14

--- pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package maruthi.springframework.sfgpetclinic.model;
 
-public class Person extends BaseEntity{
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package maruthi.springframework.sfgpetclinic.services;
 
 import maruthi.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findbyId(Long Id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
