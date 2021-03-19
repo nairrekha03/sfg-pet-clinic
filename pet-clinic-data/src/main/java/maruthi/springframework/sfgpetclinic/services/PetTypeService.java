06c371bc12d6f69642b2e63e50b9a75a8416a337
Rekha Nair <nairrekha03@gmail.com>
2021-03-18 21:02:19 -0400

    added PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package maruthi.springframework.sfgpetclinic.services;
+
+import maruthi.springframework.sfgpetclinic.model.PetType;
+import maruthi.springframework.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
