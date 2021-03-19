0184f14592a4e4b45afd6b5eed43ed27fbbf3113
Rekha Nair <nairrekha03@gmail.com>
2021-03-18 22:15:40 -0400

    Facing issue - OwnerServiceMap required a bean of type PetService that could not be found

--- pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/maruthi/springframework/sfgpetclinic/model/Pet.java
@@ -8,6 +8,8 @@ public class Pet extends BaseEntity {
     private Owner owner;
     private LocalDate birthDate;
 
+    private String name;
+
     public PetType getPetType() {
         return petType;
     }
@@ -31,4 +33,13 @@ public class Pet extends BaseEntity {
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
